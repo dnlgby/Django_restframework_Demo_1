8ed955e8b0929eb5528ca692b7e6ccfc277048e0
Daniel <dnlgby@gmail.com>
2019-08-18 22:18:45 +0300

    Added ingredients endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -14,7 +14,6 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         # Create a new user with encrypted password and return it
-        print(validated_data)
         return get_user_model().objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
